ab2deb505899c50863b600102d0d84399ff8e94f
Ankon190 <ankon190@gmail.com>
2026-06-08 23:52:28 +0600

    login design modified

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -143,12 +143,10 @@ export default function LoginPage() {
                         </div>
                     </Form>
                 </div>
-                <div className="relative my-6 space-y-6">
-                    <div className="absolute inset-0 flex items-center">
-                        <div className="w-full border-t border-gray-300"></div>
-                    </div>
-                    <div className="relative flex justify-center text-sm">
-                        <span className="px-3 bg-white text-md text-gray-500 my-5">Or continue with</span>
+                <div className="relative my-3 space-y-6">
+
+                    <div className="relative flex justify-center text-md">
+                        <span className="px-3 bg-white text-md text-gray-500 my-3">Or continue with</span>
                     </div>
                 </div>
 
@@ -156,7 +154,7 @@ export default function LoginPage() {
                     variant="bordered"
                     size="lg"
                     onClick={handleGoogleLogin}
-                    className="w-full border-gray-300  hover:bg-gray-50"
+                    className="w-full border-gray-300 hover:bg-gray-100"
                     startContent={<FaGoogle className="text-red-500" />}
                 >
                     <FcGoogle />
